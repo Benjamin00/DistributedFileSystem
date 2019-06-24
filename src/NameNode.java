e759105b7b1bb8a38ac85bd04a35523d838477cc
Vivi Song <vivisong1903@gmail.com>
2019-06-23 23:22:14 -0500

    When concatenate files, there is no space, that's a problem

---
+++ src/NameNode.java
@@ -0,0 +1,85 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class NameNode {
+	static String [][] arr = new String[40][10];	//hash table with 40*10 bc maximum 39 blocks, and maximum 10 for each
+	static int fileNum = 0;//counter for file number
+	static boolean fileFound = false;
+	
+	static String temp[] = new String[] {"D10.txt","D19.txt","D39.txt"}; //test case
+	
+	public static void main(String[] args) throws IOException {
+		Append("First.txt","Hello!");
+		//printArr(arr);	
+		Read("First.txt");
+	}
+	
+	public static void Append(String filename, String content) //filename ends in txt, centent is one big string
+	{	
+		arr[0][0] = filename;//store filename in the fist dimension
+		
+		int BlockNum = temp.length; // number of blocks assigned
+		for(int i = 1; i <= BlockNum; i++ )
+		{
+			arr[fileNum][i] = temp[i-1];
+
+		}
+		
+		//pass content to DataNode()
+		//DataNode() gives back the free block(s), format as an array [D10,D19,D39]
+		fileNum++;
+		/* test cases (hard code)
+		System.out.println(arr[0][1]);
+		System.out.println(arr[0][2]);
+		System.out.println(arr[0][3]);
+		*/
+	}
+	
+	public static void Read(String filename) throws IOException
+	{
+		FileInputStream instream = null;
+		FileOutputStream outstream = null;
+		int i=0;
+		int index = 0;
+		//if the file exist, set the flag to true
+		for(i = 0; i < arr.length; i++){  
+	        		if(arr[i][0]==filename) {
+	        			fileFound = true;
+	        			index = i;
+	        		}
+	    }		
+		for(int j = 1; j < arr[index].length; j++)
+		{
+			if(arr[index][j]!=null)
+			{
+				File infile =new File(arr[index][j]);
+		    	    File outfile =new File(filename);
+		    	    instream = new FileInputStream(infile);
+		    	    outstream = new FileOutputStream(outfile,true);
+		    	    byte[] buffer = new byte[1024];
+		    	    int length;
+		    	    while ((length = instream.read(buffer)) > 0){
+		    	    	outstream.write(buffer, 0, length);
+		    	    }
+		    	    //Closing the input/output file streams
+		    	    instream.close();
+		    	    outstream.close();
+			}
+		}
+		
+	}
+	
+	public static void printArr(String[][] example){ //print the 2d array 
+		    for(int i = 0; i < example.length; i++){  
+		        for(int j = 0; j < example[i].length; j++){ 
+		        		if(example[i][j] != null)
+		        			System.out.print(example[i][j] + "\t");
+		        }
+		        System.out.print("\n");
+		    }
+		}
+	
+
+}
