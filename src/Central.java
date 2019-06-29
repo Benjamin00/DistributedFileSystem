8489d54b23b7a7bdc44e7671047319f27cccb2c1
mikeyesudas12345 <mikeyesudas@gmail.com>
2019-06-29 13:39:50 -0500

    New Client objects

--- src/Central.java
+++ src/Central.java
@@ -8,17 +8,22 @@ public class Central extends Client {
 		String resp = c.sendMessage("Hello World");
 		
 
+		// Each of these client objects send a request
+
+
 		// For greater than 4 MB
 
 		Client d = new Client();
 		d.startConnection("127.0.0.1", 6667);
-		String messageStr = d.sendMessage("Hello");
+		String messageStr = d.sendMessage("READ D10.txt");
+		String str = d.sendMessage("APPEND D10.txt ");
 
 		// For the less than 4 MB 
 
 		Client e = new Client(); 
 		e.startConnection("127.0.0.1", 6667);
-		String messageString = e.sendMessage("Hi");
+		String messageString = e.sendMessage("READ D19.txt");
+		String msgString = e.sendMessage("APPEND D19.txt Hi");
 	}
 
 }
