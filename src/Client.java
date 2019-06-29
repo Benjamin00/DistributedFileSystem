d1e32cccaf4ab74f769beb97ff4d8545b9700229
benjamin00 <[benjaminhill00@gmail.com]>
2019-06-29 16:33:27 -0500

    cleanup on client.java file

--- src/Client.java
+++ src/Client.java
@@ -36,7 +36,8 @@ public class Client {
 //				Client c = new Client();
 //				c.startConnection("127.0.0.1", 5558);
 //				String ret = c.sendMessage(tokens[0] + " " + tokens[1] + " " + tokens[1]);	//send the read command assembly the file name.
-//				String terminate = c.sendMessage("."); 										//close the connection
+//				String terminate = c.sendMessage("."); 	
+			  //c.stopConnection     //close the connection
 			}
 			
 			else {
@@ -45,32 +46,6 @@ public class Client {
 			
 			System.out.print("::");
 		
-		
-		// Still need to send to the name node 
-
-//		
-//		Client c1 = new Client();
-//		c1.startConnection("127.0.0.1", 5558);
-//		String msg1 = c1.sendMessage("Read");
-//	    String msg2 = c1.sendMessage("Append");
-//	    String terminate = c1.sendMessage(".");
-//	    
-//	    Client c2 = new Client();
-//	    c2.startConnection("127.0.0.1", 5558);
-//		String msg11 = c2.sendMessage("Read");
-//	    String msg21 = c2.sendMessage("Write");
-//	    String terminate1 = c2.sendMessage(".");
-//		
-//	    terminate = c2.sendMessage(".");
-//	    
-//	    System.out.println("First Client: " + msg1 + " " + msg2);
-//	    System.out.println("Second Client: " + msg11 + " " + msg21);
-//	    
-//	    c1.stopConnection();
-//		c2.stopConnection();
-//		
-	
-	    
 		}
 	}
 	
