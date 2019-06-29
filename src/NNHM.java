a46e9bd89909d473be9b2f47678c379e79e36194
Vivi Song <vivisong1903@gmail.com>
2019-06-29 13:49:36 -0500

    Merged Version

--- src/NNHM.java
+++ src/NNHM.java
@@ -52,6 +52,9 @@ public class NNHM {
     }
 	
 	 private static class NameNodeHandler extends Thread {
+		 static NameNodeHandlerClient ctoD1;
+		 static NameNodeHandlerClient ctoD2;
+		 static NameNodeHandlerClient ctoD3;
 		 public class NameNodeHandlerClient {
 			    private Socket clientSocket;
 			    private PrintWriter out;
@@ -93,10 +96,11 @@ public class NNHM {
 					}
 			    }
 			}
-	        private Socket clientSocket;
+	        private static Socket clientSocket;
 	        private PrintWriter out;
 	        private BufferedReader in;
-	 	    
+	        
+
 	 	    // ---- START section that actually implements the name node handler -----
 	        public NameNodeHandler(Socket socket) {
 	            this.clientSocket = socket;
@@ -116,17 +120,29 @@ public class NNHM {
 					    
 					    
 					    //Parsing the string needs to occur here 
-					    NameNodeHandlerClient ctoD1 = new NameNodeHandlerClient();
+					    ctoD1 = new NameNodeHandlerClient();
+					    ctoD2 = new NameNodeHandlerClient();
+					    ctoD3 = new NameNodeHandlerClient();
 					    ctoD1.startConnection("127.0.0.1", 65530);//DataNode 
-					    				
-					    NameNodeHandlerClient ctoD2 = new NameNodeHandlerClient();
 					    ctoD2.startConnection("127.0.0.1", 65531); //DataNode 2
-					    
-					    NameNodeHandlerClient ctoD3 = new NameNodeHandlerClient();
 					    ctoD3.startConnection("127.0.0.1", 65532); //DataNode 2
 					     
-					    
-					    String return1 = ctoD1.sendMessage("");
+					    String file;
+					    if(inputLine.substring(0, 4).equals("Read"))
+						{
+							file = inputLine.substring(5, inputLine.length());
+							Read(file);
+						}
+						else if(inputLine.substring(0, 6).equals("Append"))
+						{
+							file = inputLine.substring(7, inputLine.indexOf(" ", 7));
+							String cont = inputLine.substring((inputLine.indexOf(" ", 7) + 1), inputLine.length());
+							Append(file,cont);
+						}
+						else
+						{
+							System.out.println("Command Failed.");
+						}
 					    
 					    if(inputLine.equals("Read")) {
 					    	Read("");
@@ -149,101 +165,98 @@ public class NNHM {
 	        //---------   The following are the "NameNode" functions -----------
 	    	public static void Append(String filename, String content) //filename ends in txt, centent is one big string
 	    	{
-//	    	   int blockNum = 0;
-//	    	   if(content.length()*2>MB)
-//	    		   blockNum = MB/content.length()+1; 
-//	    	   for(int i = 0; i < blockNum; i++)//cut string by by
-//	    	   {
-//	    		   subString.set(i,content.substring(i*MB,(i+1)*MB));
-//	    	   }
-//	    	   
-//	    	   //ask if D1 has free blocks
-//	    	   //D2...
-//	    	   //D3...
-	    		//check(String datanodeName)
-//	    	   //send blockNum & arrayList subString
-//	    	   //get back block1 from D1, NameNode pair them up and store in the hash table
-//	    	   //if gives 3 blocks back
-//	    	   List<Pair> list = new ArrayList<Pair>();
-//	    	   Pair pair1 = new Pair("D1","B2");
-//	    	   list.add(pair1);
-//	    	   
-//	    	   Pair pair2 = new Pair("D2","B3");
-//	    	   list.add(pair2);
-//	    	   
-//	    	   Pair pair3 = new Pair("D3","B4");
-//	    	   list.add(pair3);
-//
-//	    	   map.put(filename, list);//saves in the hash table
+	    	   int blockNum = 0;
+	    	   if(content.length()*2>MB)
+	    		   blockNum = MB/content.length()+1; 
+	    	   for(int i = 0; i < blockNum; i++)//cut string by by
+	    	   {
+	    		   subString.set(i,content.substring(i*MB,(i+1)*MB));
+	    	   }
+	    	   
+	    	   
+	    	   String returnID = null; //the message giving back
+	    	   List<Pair> list = new ArrayList<Pair>();
+	    	   int NumBlocksReceived = 0;
+	    	   int DNDirector = 0;
+	    	   while(NumBlocksReceived < blockNum)
+	    	   {
+	    		   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	    		   //reqest send to DN1, DN2, DN3
+	    		   //talk to DNDirect%3(th) DataNode
+	    		   //EX: 5 blockNum, request to DN1,DN2,DN3,DN1,DN2
+	    		   //if a freeblock is given back, set gotit to true
+	    		   
+	    		   if(DNDirector%3 == 0)
+	    		   {
+	    			   returnID = ctoD1.sendMessage("BlockRequest");
+	    		   }
+	    		   if(DNDirector%3 == 1)
+	    		   {
+	    			   returnID = ctoD2.sendMessage("BlockRequest");
+	    		   }
+	    		   if(DNDirector%3 == 2)
+	    		   {
+	    			   returnID = ctoD3.sendMessage("BlockRequest");
+	    		   }
+	    		   if(returnID.equals("Fail")) {DNDirector++;}
+	    		   else
+	    		   {
+	    			   Pair pair = new Pair("D1",Integer.parseInt(returnID));
+	    			   list.add(pair);
+	    			   NumBlocksReceived++;
+	    		   }   
+	    	   }
+	    	   map.put(filename, list);//saves in the hash table
 	    	   System.out.println("Called append within the handler");
 	    	}
-	    	
-	    	///////////////////////////////////////////////////////////////////////////Communication test
-	    	public static void AppendToDN1(int BNum)
-	    	{
-	    		//send block number to DN1 (EX:3)
-	    		
-	    		//get back Block id(EX:1,4,5)
-	    	}
-	    	
-	    	public static void AppendToDN2(int BNum)
-	    	{
-	    		//send block number to DN2 (EX:2)
-	    		
-	    		//get back Block id(EX:1,2)
-	    	}
-	    	
-	    	public static void AppendToDN3(int BNum)
-	    	{
-	    		//send block number to DN3 (EX:1)
-	    		
-	    		//get back Block id(EX:8)
-	    	}
-	    	
-	    	public static void ReadFromD1(int BNum)
-	    	{
-	    		//send block id to DN1 (EX:1,4,5)
-
-	    		//get back content (EX: "asdfasdfasdfadfasdfasdfasdf","asdfasdfasdfasdfasdf","asdfasdfasdfasdf")
-	    		
-	    	}
-	    	public static void ReadFromD2(int BNum)
-	    	{
-	    		//send block id to DN2 (EX:1,2)
-
-	    		//get back content (EX: "asdfasdfasdfadfasdfasdfasdf","asdfasdfasdfasdf")
-	    	}
-	    	public static void ReadFromD3(int BNum)
-	    	{
-	    		//send block id to DN3 (EX:8)
-
-	    		//get back content (EX: "asdfasdfasdfadfasdfasdfasdf")
-	    	}
-	    	
 	    	
 	    	
 	    	public static void Read(String filename)
 	    	{
-//	    		List<Pair> temp = new ArrayList<Pair>();
-//	    		if(map.containsKey(filename))//if the file is found
-//	    		{
-//	    			temp = map.get(filename);	
-//	    			for(int j = 0; j < temp.size(); j++)//add mutex to this???
-//	    			{
-//	    					//File infile =new File(arr[index][j]);
-//	    					temp.get(j).getdataNode();
-//	    					temp.get(j).getblockNode();
-//	    					//send a message to the specific data node
-//	    					//give back the centent of block note
-//	    					
-//	    					//conCat.set(j, //contnet);
-//	    			}
-//	    			String joined = String.join(" ", conCat);
-//	    			//joined is send back to client
-//	    		}else{System.out.println("The file does not exist.");
-//	    		}
+	    		String content = null;
+	    		List<Pair> temp = new ArrayList<Pair>();
+	    		if(map.containsKey(filename))//if the file is found
+	    		{
+	    			temp = map.get(filename);	
+	    			for(int j = 0; j < temp.size(); j++)//add mutex to this???
+	    			{
+	    					//File infile =new File(arr[index][j]);
+	    					temp.get(j).getdataNode();
+	    					//send a message to the specific data node
+	    					//give back the centent of block note
+	    					if(temp.get(j).getdataNode().equals("D1"))
+	    					{
+	    						content = ctoD1.sendMessage(Integer.toString(temp.get(j).getblockNode()));
+	    						conCat.set(j,content);
+	    					}
+	    					if(temp.get(j).getdataNode().equals("D2"))
+	    					{
+	    						content = ctoD2.sendMessage(Integer.toString(temp.get(j).getblockNode()));
+	    						conCat.set(j,content);
+	    					}
+	    					if(temp.get(j).getdataNode().equals("D3"))
+	    					{
+	    						content = ctoD3.sendMessage(Integer.toString(temp.get(j).getblockNode()));
+	    						conCat.set(j,content);
+	    					}
+	    			}
+	    			String joined = String.join(" ", conCat); //conCat is a string list
+	    			//joined is send back to client
+	    			output(joined);
+	    			
+	    		}else{System.out.println("The file does not exist.");
+	    		}
 	    		System.out.println("Called Read within the handler");
 	    	}
+	    	private static void output(String out) {
+	            try {
+	                PrintWriter pw = new PrintWriter(clientSocket.getOutputStream());
+	                pw.print(out);
+	                pw.flush();
+	            } catch (Exception e) {
+
+	            }
+	        }
 	 }	//END NAME NODE HANDLER
 	
 	
--- src/Pair.java
+++ src/Pair.java
@@ -2,9 +2,9 @@
 public class Pair {
 	private String DataNum;
 	//private int DataNode_Port;
-	private String BlockNum;
+	private int BlockNum;
 	
-	public Pair(String DNum, String BNum)
+	public Pair(String DNum, int BNum)
 	{
 		DataNum = DNum;
 		BlockNum = BNum;
@@ -13,6 +13,6 @@ public class Pair {
 	public String getdataNode(){return DataNum;}
 	public void setdataNode(String DNum) {DataNum = DNum;}
 	
-	public String getblockNode(){return BlockNum;}
-	public void setblockNode(String BNum) {BlockNum = BNum;}
+	public int getblockNode(){return BlockNum;}
+	public void setblockNode(int BNum) {BlockNum = BNum;}
 }
