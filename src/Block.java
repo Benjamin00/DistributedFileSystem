277ad4f86c16eda31b5895743d966484a1641890
kkunapuli <kgarot@gmail.com>
2019-06-30 00:07:47 -0500

    add some comments

--- src/Block.java
+++ src/Block.java
@@ -3,24 +3,24 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 //contains a block's information (memory location, and necessary locks)
 public class Block {
-	private String filename;
+	private String filename; //file where data is stored in a data node
 	private final ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
 	private final Lock rLock = rwLock.readLock(); //reader side of readWriteLock
 	private final Lock wLock = rwLock.writeLock(); //writer side of readWriteLock
 	
-	Block(String fname){
+	Block(String fname){ //create block with filename
 		this.filename = fname;
 	}
 	
-	String getFilename() {
+	String getFilename() { //get block filename
 		return this.filename;
 	}
 
-	Lock getRLock() {
+	Lock getRLock() { //get access to read lock
 		return this.rLock;
 	}
 
-	Lock getWLock() {
+	Lock getWLock() { //get access to write lock
 		return this.wLock;
 	}
 
